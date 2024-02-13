a3f556fa0d767bc53b7a0a5320344ef1531980a3
GevorgKartshikyan <karchikyan.00@mail.ru>
2024-02-13 16:35:43 +0400

    change favicon

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+import interactionPlugin from '@fullcalendar/interaction'
 import FullCalendar from "@fullcalendar/react";
 import {myResources,myEvents} from "../utils/data";
 const MyCalendar = () => {
   return (
       <div id='calendar'>
           <FullCalendar
-              plugins={[resourceTimelinePlugin]}
+              plugins={[resourceTimelinePlugin,interactionPlugin]}
               timeZone='UTC'
               aspectRatio={1.5}
               initialView={'resourceTimelineDay'}
@@ -16,6 +17,7 @@ const MyCalendar = () => {
                   right: 'resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth'
               }}
               editable={true}
+              eventResizableFromStart={true}
               resourceAreaHeaderContent={'Rooms'}
               resources={myResources}
               events={myEvents}
